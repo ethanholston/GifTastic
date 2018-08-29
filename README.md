afc87b07c49925d9a824c4e54fd5893f3ac8a4b7
Ethan <ethan.holston@gmail.com>
2018-08-29 18:43:14 -0400

    added animation on click, fixed 'add 12 more' button

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # GifTastic
+
+todo
+
+add cookies/favorite 
+add easy download button
+change menu item when page is selected - add data attribute to allow button to be selected and class changed. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,90 @@
 var categories = ["yes", "LOL", "happy", "nope", "slow clap", "shut up", "wink", "wtf", "fist bump", "embarassed", "drunk", "oh snap", "omg", "idgaf", "sorry", "shocked"];
 var queryURL;
 var selected;
-var numGifs = 12;
-var prevNum = 12;
+var currentPage;
+var offset = 0;
+var imgNum = 0;
 
 function getGifs(x, y){
-    queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&limit=" + y + "&api_key=dc6zaTOxFJmzC";    
+    queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&offset=" + y + "&api_key=dc6zaTOxFJmzC";    
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
         console.log(response);        
         var loadButton = "<button type='button' id='load-more' class='btn btn-secondary'>Load 12 more gifs</button>";
-        for(j=numGifs-prevNum; j<y; j++){
+        for(j=0; j<12; j++){
             var imgURL = response.data[j].images.fixed_width.url;
-            var imgNum = "#img" + j;
-            var newSpan = $("<span />").append("<img src=" + imgURL + "/>");
-            $("#img").append(newSpan);
+            var imgStill = response.data[j].images.fixed_width_still.url;
+            imgNum = "img" + j;
+            var newSpan = $("<span>");
+            var imgTag = $("<img>").attr("data-img", imgURL).attr("data-still", imgStill).attr("data-state", "still");
+            imgTag.attr("src", $(imgTag).attr("data-still"));
+            newSpan.append(imgTag);
+            $("#img").append(newSpan).attr("data-name", imgNum);
         } 
         $("#img").append(loadButton);
     });
 }
 
+function select(){
+    for(i=0; i<categories.length; i++);
+        if(selected == categories[i]);
+        $()
+}
+
 $(document).on("click", "#submit", function(){
-    $("#categories").append("<button type='button' class='btn btn-secondary menu'>" + $("#add").val() + "</button>");
-    $("#add").val('');
+    event.preventDefault();
+    if($("#add").val() != ""){
+        $("#categories").append($("<button>")
+            .attr("type", "button")
+            .addClass("btn btn-secondary menu")
+            .html("#add").val());
+        selected = $("#add").val();
+        $("#add").val('');
+        numGifs = 12;
+        $("#img").empty();
+        getGifs(selected, numGifs);
+    }
+    
 });
 
 $(document).on("click", ".menu", function(){
-    numgifs = 12;
+    offset = 12;
     selected = $(this).text();
+    $(this).removeClass("btn-secondary");
+    $(currentPage).addClass("btn-secondary");
+    currentPage = $(this).attr("id");
     $("#img").empty();
-    getGifs(selected, numGifs);
+    getGifs(selected, offset);
+    
 });
 
 $(document).on("click", "#load-more", function(){
-    prevNum = numGifs;
-    numGifs += 12;
-    if(numGifs > 25){
-        numGifs = 25;
-    }
-    getGifs(selected, numGifs);
+    offset += 12;
+    imgNum = offset;
+    // numGifs += 12;
+    getGifs(selected, offset);
     $("#load-more").detach();
 });
 
+$(document).on("click", "img", function(){
+    state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-img"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+})
+
 for(i=0; i<categories.length; i++){
-    $("#categories").append("<button type='button' class='btn btn-secondary menu'>" + categories[i] + "</button>");
+    $("#categories").prepend(
+        $("<button>").attr("type", "button")
+        .addClass("btn btn-secondary menu")
+        .text(categories[i])
+        .attr("id", categories[i]));
 }
 
-getGifs(categories[Math.floor(Math.random() * categories.length)], numGifs);
+getGifs(categories[Math.floor(Math.random() * categories.length)], 12);
